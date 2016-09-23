117bb26bd2afe9f8d82dda790d7c5509e0f3a546
Otto Streifel <ottost@microsoft.com>
2016-09-23 15:00:35 -0700

    Change listener

--- scripts/BoardControl.ts
+++ scripts/BoardControl.ts
@@ -1,31 +1,56 @@
 import Controls = require("VSS/Controls");
+import Combos = require("VSS/Controls/Combos");
 
 export interface IBoardControlOptions {
     columnValue: string;
     allowedColumnValues: string[];
-    setColumn: (columnValue: string)=>IPromise<string>;
+    setColumn: (columValue: string)=>IPromise<void>;
     laneValue: string;
     allowedLaneValues: string[];
-    setLane: (laneValue: string)=>IPromise<string>;
+    setLane: (laneValue: string)=>IPromise<void>;
     boardName: string;
     boardLink: string;
 
 }
 export class BoardControl extends Controls.Control<IBoardControlOptions> {
-    private columnValue: JQuery;
-    private laneValue: JQuery;
+    private column: Combos.Combo;
+    private lane: Combos.Combo;
     public initialize() {
-        
-        this.columnValue = $('<div/>').text(this._options.columnValue);
-        this.laneValue = $('<div/>').text(this._options.laneValue);
+        let columnOptions: Combos.IComboOptions = {
+            type: 'list',
+            source: this._options.allowedColumnValues,
+            value: this._options.columnValue,
+            change: () => {
+                let columnValue = this.lane.getInputText();
+                console.log(`Setting the column value to ${columnValue}`)
+                this._options.setColumn(columnValue)
+                .then(() => {
+                    console.log(`Set the column value to ${columnValue}`)
+                });
+            }
+
+        };
+        let laneOptions: Combos.IComboOptions = {
+            type: 'list',
+            source: this._options.allowedLaneValues,
+            value: this._options.laneValue,
+            change: () => {
+                let laneValue = this.lane.getInputText();
+                console.log(`Setting the lane value to ${laneValue}`);
+                this._options.setLane(laneValue)
+                .then(() => {
+                    console.log(`Set the lane value to ${laneValue}`);
+                });
+            }
+        };
+
         let boardLink = $('<a/>').text(this._options.boardName)
             .attr("href", this._options.boardLink); 
-        this._element
-            .append(boardLink)
-            .append($('<h4/>').text('Board Column'))
-            .append(this.columnValue)
-            .append($('<h4/>').text('Board Lane'))
-            .append(this.laneValue);
+        this._element.append(boardLink);
+        this._element.append($('<h4/>').text('Board Column'));
+        this.column = <Combos.Combo>Controls.BaseControl.createIn(Combos.Combo, this._element, columnOptions);
+        this._element.append($('<h4/>').text('Board Lane'));
+        this.lane = <Combos.Combo>Controls.BaseControl.createIn(Combos.Combo, this._element, laneOptions);
     }
 
 }
--- scripts/app.ts
+++ scripts/app.ts
@@ -1,19 +1,20 @@
 import {BoardControl, IBoardControlOptions} from "./BoardControl";
 import Controls = require("VSS/Controls");
+import Q = require("q");
 
 let options: IBoardControlOptions = {
     columnValue: "Sample column value",
     allowedColumnValues: ["Sample column value", "Sample column value2"],
     setColumn: (columnValue: string)=>{
-        var defer = Q.defer<string>();
-        defer.resolve(columnValue);
+        var defer = Q.defer<void>();
+        defer.resolve();
         return defer.promise;
     },
     laneValue: "Sample lane value",
     allowedLaneValues: ["Sample lane value", "Sample lane value2"],
-    setLane: (columnValue: string)=>{
-        var defer = Q.defer<string>();
-        defer.resolve(columnValue);
+    setLane: (laneValue: string)=>{
+        var defer = Q.defer<void>();
+        defer.resolve();
         return defer.promise;
     },
     boardName: "Board name",
