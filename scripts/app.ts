3d22eb2d995b87efaf172238907b65cc0c1d5fbe
Otto Streifel <ottost@microsoft.com>
2016-09-25 13:58:33 -0700

    Refresh and update

--- scripts/app.ts
+++ scripts/app.ts
@@ -2,12 +2,19 @@ import {BoardControl, IBoardControlOptions} from "./BoardControl";
 import Controls = require("VSS/Controls");
 import Q = require("q");
 import {getBoardOptions} from "./getBoardOptions";
+import {IWorkItemFieldChangedArgs, IWorkItemNotificationListener} from "TFS/WorkItemTracking/ExtensionContracts";
 
+Controls.Enhancement.registerEnhancement(BoardControl, '.board-control', getBoardOptions());
 
-let optionPromise: IPromise<IBoardControlOptions> = getBoardOptions();
-
-Controls.Enhancement.registerEnhancement(BoardControl, '.board-control', optionPromise);
+let refresh = () => {
+    let container = $('.board-control')
+    container.empty();
+    Controls.BaseControl.createIn(BoardControl, container, getBoardOptions());
+}
 
 // Register context menu action provider
 const publisherId = VSS.getExtensionContext().publisherId;
-VSS.register(`${ publisherId }.board-group.board-work-item-form-group`, {});
+VSS.register(`${ publisherId }.board-group.board-work-item-form-group`, {
+    onRefreshed: refresh,
+    onReset: refresh
+});
