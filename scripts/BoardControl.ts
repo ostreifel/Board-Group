a5a8fd710fb2fd5c9a30c1c393b55af3cb4f73ea
Otto Streifel <ottost@microsoft.com>
2016-12-12 09:35:43 -0800

    tslint changes

--- scripts/BoardControl.ts
+++ scripts/BoardControl.ts
@@ -1,28 +1,15 @@
 import { Control, BaseControl } from "VSS/Controls";
 import { Combo, IComboOptions } from "VSS/Controls/Combos";
-import { WorkItemFormService, IWorkItemFormService } from "TFS/WorkItemTracking/Services";
-import { IBoardControlOptions } from "./BoardControl";
 import Q = require("q");
 import { getClient as getWorkClient } from "TFS/Work/RestClient";
-import { BoardReference, Board, BoardColumnType } from "TFS/Work/Contracts";
+import { Board, BoardColumnType } from "TFS/Work/Contracts";
 import { getClient as getWITClient } from "TFS/WorkItemTracking/RestClient";
 import { TeamContext } from "TFS/Core/Contracts";
-import { IWorkItemChangedArgs, IWorkItemFieldChangedArgs, IWorkItemLoadedArgs } from "TFS/WorkItemTracking/ExtensionContracts";
+import { IWorkItemChangedArgs, IWorkItemLoadedArgs } from "TFS/WorkItemTracking/ExtensionContracts";
 import { JsonPatchDocument, JsonPatchOperation, Operation } from "VSS/WebApi/Contracts";
 
-export interface IBoardControlOptions {
-    columnValue: string;
-    allowedColumnValues: string[];
-    setColumn: (columValue: string) => IPromise<boolean>;
-    laneValue: string;
-    allowedLaneValues: string[];
-    setLane: (laneValue: string) => IPromise<boolean>;
-    boardName: string;
-    boardUrl: string;
-}
-
 export class BoardControl extends Control<{}> {
-    //Data
+    // data
     private wiId: number;
     private board: Board;
     private columnValue: string;
@@ -30,7 +17,7 @@ export class BoardControl extends Control<{}> {
     private doneValue: boolean;
     private workItemType: string;
 
-    //UI
+    // ui
     private columnInput: Combo;
     private laneInput: Combo;
     private doneInput: Combo;
@@ -38,12 +25,6 @@ export class BoardControl extends Control<{}> {
         VSS.resize();
     }
     private findAllBoards() {
-        const optionsDeferred: Q.Deferred<IBoardControlOptions> = Q.defer<IBoardControlOptions>();
-
-        const rejectOnError = (error: TfsError | string) => {
-            optionsDeferred.reject(error)
-        };
-
         const teamContext: TeamContext = {
             project: VSS.getWebContext().project.name,
             projectId: VSS.getWebContext().project.id,
@@ -54,7 +35,7 @@ export class BoardControl extends Control<{}> {
             (boardReferences) => {
                 Q.all(boardReferences.map(b => getWorkClient().getBoard(teamContext, b.id))).then(
                     (boards) => this.findAssociatedBoard(boards)
-                )
+                );
             }
         );
     }
@@ -74,15 +55,15 @@ export class BoardControl extends Control<{}> {
                     this.updateNoBoard();
                 } else {
                     const board = fieldMapping[Object.keys(workItem.fields)[1]];
-                    this.updateForBoard(workItem.fields, board)
+                    this.updateForBoard(workItem.fields, board);
                 }
             }
-        )
+        );
     }
 
     private updateNoBoard() {
         this.board = null;
-        this._element.html('<div>No associated board for current team</div>');
+        this._element.html("<div>No associated board for current team</div>");
     }
 
     private updateForBoard(fields: { [refName: string]: any }, board: Board) {
@@ -107,36 +88,35 @@ export class BoardControl extends Control<{}> {
             }
         };
 
-        const accountName = VSS.getWebContext().account.name;
         const projectName = VSS.getWebContext().project.name;
         const teamName = VSS.getWebContext().team.name;
         const uri = VSS.getWebContext().host.uri;
         const boardUrl = `${uri}${projectName}/${teamName}/_backlogs/board/${board.name}`;
 
-        this._element.html('')
-        const boardLink = $('<a/>').text(board.name)
+        this._element.html("");
+        const boardLink = $("<a/>").text(board.name)
             .attr({
                 href: boardUrl,
                 target: "_parent"
             });
 
-        this._element.append(boardLink).append($('<br><br>'));
+        this._element.append(boardLink).append($("<br><br>"));
         if (this.columnValue) {
-            this._element.append($('<label/>').addClass('workitemcontrol-label').text('Board Column'));
+            this._element.append($("<label/>").addClass("workitemcontrol-label").text("Board Column"));
             this.columnInput = <Combo>BaseControl.createIn(Combo, this._element, columnOptions);
         } else {
             this.columnInput = null;
         }
-        this._element.append('<div class="lane-input" />');
-        this._element.append('<div class="done-input" />');
-        this._element.append('<div>Board changes are saved immediately.</div>')
+        this._element.append(`<div class="lane-input" />`);
+        this._element.append(`<div class="done-input" />`);
+        this._element.append("<div>Board changes are saved immediately.</div>");
         this.updateLaneInput();
         this.updateDoneInput();
     }
 
     private updateLaneInput() {
-        const laneElem = $('.lane-input', this._element);
-        laneElem.html('');
+        const laneElem = $(".lane-input", this._element);
+        laneElem.html("");
         const columnValue = this.getColumnInputValue();
         const column = this.board.columns.filter((c) => c.name === columnValue)[0];
         if (this.board.rows.length > 1
@@ -144,8 +124,8 @@ export class BoardControl extends Control<{}> {
             && column.columnType !== BoardColumnType.Outgoing) {
             const boardControl = this;
             const laneOptions: IComboOptions = {
-                value: this.rowValue || '(Default Lane)',
-                source: this.board.rows.map((r) => r.name || '(Default Lane)'),
+                value: this.rowValue || "(Default Lane)",
+                source: this.board.rows.map((r) => r.name || "(Default Lane)"),
                 change: function () {
                     VSS.resize();
                     const box: Combo = this;
@@ -157,7 +137,7 @@ export class BoardControl extends Control<{}> {
                     maxRowCount: 5
                 }
             };
-            laneElem.append($('<label/>').addClass('workitemcontrol-label').text('Board Lane'));
+            laneElem.append($("<label/>").addClass("workitemcontrol-label").text("Board Lane"));
             this.laneInput = <Combo>BaseControl.createIn(Combo, laneElem, laneOptions);
         } else {
             this.laneInput = null;
@@ -167,8 +147,8 @@ export class BoardControl extends Control<{}> {
     private updateDoneInput() {
         const boardControl = this;
         const doneOptions: IComboOptions = {
-            value: this.doneValue ? 'True' : 'False',
-            source: ['True', 'False'],
+            value: this.doneValue ? "True" : "False",
+            source: ["True", "False"],
             change: function () {
                 const box: Combo = this;
                 if (box.getSelectedIndex() > -1) {
@@ -179,10 +159,10 @@ export class BoardControl extends Control<{}> {
         const columnValue = this.getColumnInputValue();
         const isSplit = this.board.columns.filter((c) => c.name === columnValue)[0].isSplit;
 
-        const doneElem = $('.done-input', this._element);
-        doneElem.html('');
+        const doneElem = $(".done-input", this._element);
+        doneElem.html("");
         if (isSplit) {
-            doneElem.append($('<label/>').addClass('workitemcontrol-label').text('Done'));
+            doneElem.append($("<label/>").addClass("workitemcontrol-label").text("Done"));
             this.doneInput = <Combo>BaseControl.createIn(Combo, doneElem, doneOptions);
         } else {
             this.doneInput = null;
@@ -214,8 +194,8 @@ export class BoardControl extends Control<{}> {
     public onLoaded(loadedArgs: IWorkItemLoadedArgs) {
         if (!loadedArgs.isNew) {
             this.wiId = loadedArgs.id;
-            this._element.html('');
-            this._element.append($('<div/>').text('Looking for associated board...'));
+            this._element.html("");
+            this._element.append($("<div/>").text("Looking for associated board..."));
             this.findAllBoards();
         }
     }
@@ -231,15 +211,15 @@ export class BoardControl extends Control<{}> {
         );
     }
 
-    private save(field: 'columnField' | 'rowField', val: string);
-    private save(field: 'doneField', val: boolean);
-    private save(field: 'columnField' | 'rowField' | 'doneField', val: string | boolean) {
+    private save(field: "columnField" | "rowField", val: string);
+    private save(field: "doneField", val: boolean);
+    private save(field: "columnField" | "rowField" | "doneField", val: string | boolean) {
         if (!this.board) {
             console.warn(`Save called on ${field} with ${val} when board not set`);
             return;
         }
         const patchDocument: JsonPatchDocument & JsonPatchOperation[] = [];
-        if (field === 'rowField' && !val) {
+        if (field === "rowField" && !val) {
             patchDocument.push(<JsonPatchOperation>{
                 op: Operation.Remove,
                 path: `/fields/${this.board.fields[field].referenceName}`
--- scripts/boardGroup.ts
+++ scripts/boardGroup.ts
@@ -1,20 +1,19 @@
-import {BoardControl, IBoardControlOptions} from "./BoardControl";
+import {BoardControl} from "./BoardControl";
 import Controls = require("VSS/Controls");
-import Q = require("q");
-import {IWorkItemFieldChangedArgs, IWorkItemNotificationListener} from "TFS/WorkItemTracking/ExtensionContracts";
-import { WorkItemFormService, IWorkItemFormService } from "TFS/WorkItemTracking/Services";
+import { IWorkItemNotificationListener} from "TFS/WorkItemTracking/ExtensionContracts";
+import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
 
-// Save on ctr + s
-$(window).bind('keydown', function(event) {
+// save on ctr + s
+$(window).bind("keydown", function(event: JQueryEventObject) {
     if (event.ctrlKey || event.metaKey) {
-        if (String.fromCharCode(event.which).toLowerCase() === 's') {
+        if (String.fromCharCode(event.which).toLowerCase() === "s") {
             event.preventDefault();
             WorkItemFormService.getService().then((service)=>service.beginSaveWorkItem($.noop, $.noop));
         }
     }
 });
 
-const boardControl = <BoardControl>Controls.BaseControl.createIn(BoardControl, $('.board-control'));
+const boardControl = <BoardControl>Controls.BaseControl.createIn(BoardControl, $(".board-control"));
 
 const contextData = <IWorkItemNotificationListener>{};
 contextData.onSaved = (savedEventArgs) => boardControl.onSaved(savedEventArgs);
