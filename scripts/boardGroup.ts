b76a95ceb0e1a05a4b4b3d324465b2ffd52e72d7
Otto Streifel <ottost@microsoft.com>
2016-12-16 17:44:42 -0800

    formatting

--- scripts/boardGroup.ts
+++ scripts/boardGroup.ts
@@ -1,22 +1,23 @@
-import {BoardControl} from "./BoardControl";
+import { BoardControl } from "./BoardControl";
 import Controls = require("VSS/Controls");
-import { IWorkItemNotificationListener} from "TFS/WorkItemTracking/ExtensionContracts";
+import { IWorkItemNotificationListener } from "TFS/WorkItemTracking/ExtensionContracts";
 import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
 
 // save on ctr + s
-$(window).bind("keydown", function(event: JQueryEventObject) {
+$(window).bind("keydown", function (event: JQueryEventObject) {
     if (event.ctrlKey || event.metaKey) {
         if (String.fromCharCode(event.which).toLowerCase() === "s") {
             event.preventDefault();
-            WorkItemFormService.getService().then((service)=>service.beginSaveWorkItem($.noop, $.noop));
+            WorkItemFormService.getService().then((service) => service.beginSaveWorkItem($.noop, $.noop));
         }
     }
 });
 
 const boardControl = <BoardControl>Controls.BaseControl.createIn(BoardControl, $(".board-control"));
 
-const contextData = <IWorkItemNotificationListener>{};
-contextData.onSaved = (savedEventArgs) => boardControl.onSaved(savedEventArgs);
-contextData.onRefreshed = () => boardControl.onRefreshed();
-contextData.onLoaded = (loadedArgs) => boardControl.onLoaded(loadedArgs);
+const contextData: Partial<IWorkItemNotificationListener> = {
+    onSaved: (savedEventArgs) => boardControl.onSaved(savedEventArgs),
+    onRefreshed: () => boardControl.onRefreshed(),
+    onLoaded: (loadedArgs) => boardControl.onLoaded(loadedArgs)
+};
 VSS.register(VSS.getContribution().id, contextData);
