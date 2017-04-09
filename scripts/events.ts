1a71400b89ce5225f1cae954649a10ed465d4c1c
Otto Streifel <ottost@microsoft.com>
2017-04-09 14:27:37 -0700

    v2.3.52 better info hover

--- scripts/events.ts
+++ scripts/events.ts
@@ -8,6 +8,7 @@ export interface IMeasurements {
 export function trackEvent(name: string, properties?: IProperties, measurements?: IMeasurements) {
     const insights = getInsights();
     if (insights) {
+        properties = {...(properties || {}), host: VSS.getWebContext().host.authority}
         insights.trackEvent(name, properties, measurements);
         insights.flush();
     }
